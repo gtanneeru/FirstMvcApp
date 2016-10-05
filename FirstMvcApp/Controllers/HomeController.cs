3f0a993d5dcd49b4763030343ebdb4ac8ce18f51
gtanneeru <gtanneeru@gmail.com>
2016-10-04 22:12:13 -0400

    web application

---
+++ FirstMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMvcApp.Models;
+
+namespace FirstMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour<12?"Good Morning":"Good Afternoon";
+            return View();
+        }
+        public ViewResult Gayu()
+        { 
+            ViewBag.message = "This is an Example";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestresponse)
+        {
+            return View("Thanks", guestresponse);
+        }
+    }
+}
---
+++ FirstMvcApp/Models/GuestResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using FirstMvcApp;
+
+namespace FirstMvcApp.Models
+{
+    public class GuestResponse
+    {
+        [Required (ErrorMessage= "Please Enter your Name")]
+        public string Name { get; set; }
+       [Required (ErrorMessage ="Please enter your email address")]
+       [RegularExpression(".+\\@.+\\..+", ErrorMessage ="Please Enter a valid email Address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Please enter Your Phone number")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage ="Please Specify whether u ar attending?")]
+        public bool? WillAttend { get; set; }  //? allows null
+
+    }
+}
